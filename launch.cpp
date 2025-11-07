f1d26365b983b1a76126e5b0bb186bb3c192c7e0
kaisen <qwkparaiso@tip.edu.ph>
2025-11-07 08:35:44 +0800

    ADDED READING AND WRITING OF DATABASE

--- launch.cpp
+++ launch.cpp
@@ -7,8 +7,8 @@ using namespace std;
 // define a structure to hold room scheduling information
 struct roomSchedule {
 	string month;
-	int day = 0;
-	int year = 0;
+	string day;
+	string year;
 	string startTime;
 	string endTime;
 };
@@ -18,14 +18,8 @@ void launchApp() {
 		cout << "Launching the Frontrooms Scheduling Management System..." << endl << endl;
 		
 		roomSchedule r;
+		ifstream inFile;
 
-		// open the file containing room schedules
-		ifstream inFile("ROOMS.txt");
-
-		// check if the file opened successfully
-		if (!inFile.is_open()) {  
-			cout << "Error opening file!" << endl;
-		}
 
 		// welcome message
 		cout << "--------------------------------------------------------" << endl;
@@ -41,57 +35,125 @@ void launchApp() {
 		cin >> r.year;
 
 		// read through the file and check for matching schedules
+
 		string line;
 		int matchCount = 0;
 
 		// 
+		inFile.open("3201.txt");
 		while (getline(inFile, line)) {
-			
+
 			// check for month, day, and year matches
 			if (line.find(r.month) != string::npos) {
-				
+
 			}
 			else {
 				matchCount++;
 			}
-			if (line.find(to_string(r.day)) != string::npos) {
-				
+			if (line.find(r.day) != string::npos) {
+
 			}
 			else {
 				matchCount++;
 			}
-			if (line.find(to_string(r.year)) != string::npos) {
-				
+			if (line.find(r.year) != string::npos) {
+
 			}
 			else {
 				matchCount++;
 			}
 		}
+		inFile.close();
 
-		inFile.clear();                 // Clear EOF flag
-		inFile.seekg(0);    // Move to the beginning of the file
-		
+		inFile.open("3202.txt");
 		while (getline(inFile, line)) {
-			bool print = false;
-			if (line.find("RoomID:") != string::npos) {
-				print = true;
-				cout << endl;
-			}
 
-			if (print == true) {
-				cout << line << endl;
-				if (line.empty())  // stop at blank line
-					print = false;
+			// check for month, day, and year matches
+			if (line.find(r.month) != string::npos) {
+
+			}
+			else {
+				matchCount++;
+			}
+			if (line.find(r.day) != string::npos) {
+
+			}
+			else {
+				matchCount++;
+			}
+			if (line.find(r.year) != string::npos) {
+
+			}
+			else {
+				matchCount++;
 			}
 		}
+		inFile.close();
+
+		inFile.open("3203.txt");
+		while (getline(inFile, line)) {
+
+			// check for month, day, and year matches
+			if (line.find(r.month) != string::npos) {
+
+			}
+			else {
+				matchCount++;
+			}
+			if (line.find(r.day) != string::npos) {
+
+			}
+			else {
+				matchCount++;
+			}
+			if (line.find(r.year) != string::npos) {
+
+			}
+			else {
+				matchCount++;
+			}
+		}
+		inFile.close();
+
+		inFile.open("3204.txt");
+		while (getline(inFile, line)) {
+
+			// check for month, day, and year matches
+			if (line.find(r.month) != string::npos) {
+
+			}
+			else {
+				matchCount++;
+			}
+			if (line.find(r.day) != string::npos) {
+
+			}
+			else {
+				matchCount++;
+			}
+			if (line.find(r.year) != string::npos) {
+
+			}
+			else {
+				matchCount++;
+			}
+		}
+
 
 		inFile.close();
 
-		ofstream outFile("ROOMS.txt", ios::out);
 
-		string roomID;
-		cout << "Please enter the Room ID you want to reserve: ";
+		int roomID = 0;
+		ofstream outFile;
+
+		cout << "Please enter the room you desire to use: ";
 		cin >> roomID;
+
+		switch (roomID) {
+
+		case 3201: {
+
+			outFile.open("3201.txt", ios::out);
 
 			if (line.find(roomID)) {
 				outFile << "RoomID: " << roomID << endl;
@@ -101,6 +163,55 @@ void launchApp() {
 				outFile << "StartTime: " << r.startTime << endl;
 				outFile << "EndTime: " << r.endTime << endl;
 			}
-		outFile.close();
-		
+			outFile.close();
+		} break;
+
+
+		case 3202: {
+
+			outFile.open("3202.txt", ios::out);
+
+			if (line.find(roomID)) {
+				outFile << "RoomID: " << roomID << endl;
+				outFile << "ResMonth: " << r.month << endl;
+				outFile << "ResDay: " << r.day << endl;
+				outFile << "ResYear: " << r.year << endl;
+				outFile << "StartTime: " << r.startTime << endl;
+				outFile << "EndTime: " << r.endTime << endl;
+			}
+			outFile.close();
+		} break;
+
+
+		case 3203: {
+
+			outFile.open("3203.txt", ios::out);
+
+			if (line.find(roomID)) {
+				outFile << "RoomID: " << roomID << endl;
+				outFile << "ResMonth: " << r.month << endl;
+				outFile << "ResDay: " << r.day << endl;
+				outFile << "ResYear: " << r.year << endl;
+				outFile << "StartTime: " << r.startTime << endl;
+				outFile << "EndTime: " << r.endTime << endl;
+			}
+			outFile.close();
+		} break;
+
+
+		case 3204: {
+
+			outFile.open("3204.txt", ios::out);
+			if (line.find(roomID)) {
+				outFile << "RoomID: " << roomID << endl;
+				outFile << "ResMonth: " << r.month << endl;
+				outFile << "ResDay: " << r.day << endl;
+				outFile << "ResYear: " << r.year << endl;
+				outFile << "StartTime: " << r.startTime << endl;
+				outFile << "EndTime: " << r.endTime << endl;
+			}
+			outFile.close();
+		} break;
+
+		}
 }
